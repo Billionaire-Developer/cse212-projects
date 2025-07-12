266dd660ee49e92770f7af6262515b2b6dca33bc
Billionaire_Developer <lawanijeremiah23@gmail.com>
2025-07-12 23:42:17 +0100

    cse212

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,29 +1,91 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-// TODO Problem 2 - Write and run test cases and fix the code to match requirements.
+using System;
 
 [TestClass]
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Single highest priority item should be dequeued first.
+    // Expected Result: "A" is returned as it has the highest priority.
+    // Defect(s) Found: None
+    public void TestPriorityQueue_SingleHighestPriority()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var queue = new PriorityQueue();
+        queue.Enqueue("A", 5);
+        queue.Enqueue("B", 2);
+        queue.Enqueue("C", 3);
+        Assert.AreEqual("A", queue.Dequeue());
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: Multiple items with the same highest priority.
+    // Expected Result: "X" is returned first due to FIFO rule.
+    // Defect(s) Found: None
+    public void TestPriorityQueue_MultipleSamePriority()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var queue = new PriorityQueue();
+        queue.Enqueue("X", 10);
+        queue.Enqueue("Y", 10);
+        queue.Enqueue("Z", 5);
+        Assert.AreEqual("X", queue.Dequeue());
+        Assert.AreEqual("Y", queue.Dequeue());
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Dequeue all items in correct order of priority.
+    // Expected Result: "High", then "Mid", then "Low".
+    // Defect(s) Found: None
+    public void TestPriorityQueue_DequeueAllInOrder()
+    {
+        var queue = new PriorityQueue();
+        queue.Enqueue("Low", 1);
+        queue.Enqueue("Mid", 5);
+        queue.Enqueue("High", 10);
+        Assert.AreEqual("High", queue.Dequeue());
+        Assert.AreEqual("Mid", queue.Dequeue());
+        Assert.AreEqual("Low", queue.Dequeue());
+    }
+
+    [TestMethod]
+    // Scenario: Attempt to dequeue from an empty queue.
+    // Expected Result: Throws InvalidOperationException.
+    // Defect(s) Found: None
+    public void TestPriorityQueue_EmptyDequeueThrows()
+    {
+        var queue = new PriorityQueue();
+        Assert.ThrowsException<InvalidOperationException>(() => queue.Dequeue());
+    }
+
+    [TestMethod]
+    // Scenario: Items with descending priorities should be dequeued in priority order.
+    // Expected Result: "Four", "Three", "Two", "One".
+    // Defect(s) Found: None
+    public void TestPriorityQueue_DescendingPriorities()
+    {
+        var queue = new PriorityQueue();
+        queue.Enqueue("One", 1);
+        queue.Enqueue("Two", 2);
+        queue.Enqueue("Three", 3);
+        queue.Enqueue("Four", 4);
+
+        Assert.AreEqual("Four", queue.Dequeue());
+        Assert.AreEqual("Three", queue.Dequeue());
+        Assert.AreEqual("Two", queue.Dequeue());
+        Assert.AreEqual("One", queue.Dequeue());
+    }
+
+    [TestMethod]
+    // Scenario: All items have the same priority.
+    // Expected Result: Items dequeued in the same order they were enqueued (FIFO).
+    // Defect(s) Found: None
+    public void TestPriorityQueue_AllSamePriorityFIFO()
+    {
+        var queue = new PriorityQueue();
+        queue.Enqueue("A", 3);
+        queue.Enqueue("B", 3);
+        queue.Enqueue("C", 3);
+        Assert.AreEqual("A", queue.Dequeue());
+        Assert.AreEqual("B", queue.Dequeue());
+        Assert.AreEqual("C", queue.Dequeue());
+    }
 }
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,23 +1,26 @@
 /// <summary>
-/// This queue is circular.  When people are added via AddPerson, then they are added to the 
-/// back of the queue (per FIFO rules).  When GetNextPerson is called, the next person
-/// in the queue is saved to be returned and then they are placed back into the back of the queue.  Thus,
-/// each person stays in the queue and is given turns.  When a person is added to the queue, 
-/// a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
-/// less than they will stay in the queue forever.  If a person is out of turns then they will 
-/// not be added back into the queue.
+/// This queue is circular. When people are added via AddPerson, they are added to the 
+/// back of the queue (FIFO). When GetNextPerson is called, the next person
+/// is removed from the front and returned. They are placed at the back again only if:
+/// - They have more than 1 turn (decrement their turns and re-enqueue)
+/// - They have 0 or fewer turns (infinite turns, re-enqueue unchanged)
+/// If their turns were exactly 1, they are not re-added to the queue.
 /// </summary>
 public class TakingTurnsQueue
 {
     private readonly PersonQueue _people = new();
 
+    /// <summary>
+    /// Gets the current number of people in the queue.
+    /// </summary>
     public int Length => _people.Length;
 
     /// <summary>
-    /// Add new people to the queue with a name and number of turns
+    /// Add a new person to the queue with a name and a number of turns.
+    /// If turns is 0 or less, the person will stay in the queue indefinitely.
     /// </summary>
-    /// <param name="name">Name of the person</param>
-    /// <param name="turns">Number of turns remaining</param>
+    /// <param name="name">The name of the person</param>
+    /// <param name="turns">The number of turns (0 or less = infinite)</param>
     public void AddPerson(string name, int turns)
     {
         var person = new Person(name, turns);
@@ -25,29 +28,38 @@ public class TakingTurnsQueue
     }
 
     /// <summary>
-    /// Get the next person in the queue and return them. The person should
-    /// go to the back of the queue again unless the turns variable shows that they 
-    /// have no more turns left.  Note that a turns value of 0 or less means the 
-    /// person has an infinite number of turns.  An error exception is thrown 
-    /// if the queue is empty.
+    /// Returns the next person in the queue.
+    /// Re-enqueues the person based on their remaining turns:
+    /// - Infinite turns (≤ 0): person is re-added without change
+    /// - Finite turns (> 1): decremented and re-added
+    /// - Exactly 1 turn: returned without re-adding
+    /// Throws an exception if the queue is empty.
     /// </summary>
+    /// <returns>The next person in the queue</returns>
+    /// <exception cref="InvalidOperationException">If the queue is empty</exception>
     public Person GetNextPerson()
     {
         if (_people.IsEmpty())
         {
             throw new InvalidOperationException("No one in the queue.");
         }
-        else
-        {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1)
-            {
-                person.Turns -= 1;
-                _people.Enqueue(person);
-            }
 
-            return person;
+        Person person = _people.Dequeue();
+
+        if (person.Turns <= 0)
+        {
+            // Infinite turns, re-enqueue as-is
+            _people.Enqueue(person);
         }
+        else if (person.Turns > 1)
+        {
+            // Decrement and re-enqueue
+            person.Turns -= 1;
+            _people.Enqueue(person);
+        }
+        // If person.Turns == 1, do not re-enqueue
+
+        return person;
     }
 
     public override string ToString()
